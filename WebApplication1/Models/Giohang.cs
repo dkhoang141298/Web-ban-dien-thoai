4dc2959fe4b7e70e0802cdc432d9ef5433ae4481
YuSuBui <buicongsum2001@gmail.com>
2020-05-31 15:02:52 +0700

    Giao diện
    Mình đã làm xong giao diện

---
+++ WebApplication1/Models/Giohang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Giohang
+    {
+        DataQLBanDTDDDataContext data = new DataQLBanDTDDDataContext();
+        public int iMaSP { get; set; }
+        public string sTenSP { get; set; }
+        public string sAnhbia { get; set; }
+        public Double dDongia { get; set; }
+        public int iSoluong { get; set; }
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+        public Giohang(int MaSP)
+        {
+            iMaSP = MaSP;
+            SANPHAM sp = data.SANPHAMs.Single(n => n.MaSP == iMaSP);
+            sTenSP = sp.TenSP;
+            sAnhbia = sp.Anhbia;
+            dDongia = double.Parse(sp.Giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
